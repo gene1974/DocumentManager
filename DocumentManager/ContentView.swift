79659ea0199454fd13f2a5d653434d1d6235cec4
gene1974 <842491684@qq.com>
2026-03-22 22:46:27 +0800

    Initial Commit

---
+++ DocumentManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DocumentManager
+//
+//  Created by Yingzhuo Huang on 2026/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DocumentManager/DocumentManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentManagerApp.swift
+//  DocumentManager
+//
+//  Created by Yingzhuo Huang on 2026/3/22.
+//
+
+import SwiftUI
+
+@main
+struct DocumentManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
